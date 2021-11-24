c9cfb306be600fefaab9caed5f3897393571422a
PatrickJurt <74709148+PatrickJurt@users.noreply.github.com>
2021-11-24 16:08:10 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SPAIN</title>
+    <link rel="stylesheet" href="index.css" type="text/css">
+  </head>
+  <body>
+    <header>
+      <h1>sPAIN</h1>
+      <nav>
+        <ul>
+          <li><a href="index.php">Homepage</a></li>
+          <li><a href="wiki.php">Wiki</a></li>
+          <li><a href="softwaretools.php">Softwaretools</a></li>
+          <li><a href="videos.php">Vidz</a></li>
+          <li><a href="impressum.php">Impressum</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SPAIN</title>
+    <link rel="stylesheet" href="index.css" type="text/css">
+  </head>
+  <body>
+    <?php include 'header.php' ?>
+    <div class="content">
+      <p>Lorem Ipsum dolor set amet....</p>
+    </div>
+  </body>
+</html>
